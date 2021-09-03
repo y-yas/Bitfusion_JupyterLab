4f8523a0882d0349a43c18e493f4cf422eb74fa6
y-yas <55268781+y-yas@users.noreply.github.com>
2021-09-03 09:40:34 +0900

    main

--- setup_Jupyter.sh
+++ setup_Jupyter.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 distribution=$". /etc/os-release;echo $VERSION_ID"
 gitdir=$"~/Bitfusion_JupyterLab"
-
+mkdir tmp
 sudo apt-get update
 
 sudo apt install -y python3-pip
 sudo pip3 install jupyterlab
-ipython kernel install --prefix "~/tmp"
+ipython kernel install --prefix ~/tmp
 
 
 cd "~/tmp/share/jupyter/kernels/"
