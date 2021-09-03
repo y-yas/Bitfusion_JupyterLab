6d92105fe634d83b05aae82b45d94ecd770fd76b
y-yas <55268781+y-yas@users.noreply.github.com>
2021-09-03 09:08:23 +0900

    main

--- setup_Jupyter.sh
+++ setup_Jupyter.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt install -y python3-pip
 sudo pip3 install jupyterlab
 ipython kernel install --prefix ~/tmp
-
+ 
 
 cd ~/tmp/share/jupyter/kernels/
 mv python3/ bitfusion-basic
