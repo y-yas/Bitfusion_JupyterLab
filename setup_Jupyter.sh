718207765b1c447365bb3121ba46e39ddf18c059
y-yas <55268781+y-yas@users.noreply.github.com>
2021-09-03 09:48:30 +0900

    main

--- setup_Jupyter.sh
+++ setup_Jupyter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 distribution=$". /etc/os-release;echo $VERSION_ID"
 gitdir=$"~/Bitfusion_JupyterLab"
-mkdir tmp
+mkdir ~/tmp
 sudo apt-get update
 
 sudo apt install -y python3-pip
@@ -9,9 +9,9 @@ sudo pip3 install jupyterlab
 ipython kernel install --prefix ~/tmp
 
 
-cd "~/tmp/share/jupyter/kernels/"
+cd ~/tmp/share/jupyter/kernels/
 mv python3/ bitfusion-basic
-cat $gitdir/Integrated_jupyterLab/kernel.json > kernel.json
+cat $gitdir/Integrated_jupyterLab/kernel.json > ./kernel.json
 
 
 jupyter kernelspec install --user tmp/share/jupyter/kernels/bitfusion-basic/
